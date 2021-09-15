0f06e89cbd4b9ae2f0dea12314bb7a844d1f5fdb
wuixwui <seoweet21@gmail.com>
2021-09-15 14:27:42 +0900

    feat: json 데이터 연결

--- src/js/app.js
+++ src/js/app.js
@@ -24,3 +24,21 @@ const spendingDetailToggleBtn = document.querySelector(
 spendingDetailToggleBtn.addEventListener('click', () => {
   spendingDetail.classList.toggle('up');
 });
+
+// chart
+const bankDataRequestURL = 'https://syoon0624.github.io/json/test.json';
+
+const BankDataRequest = new XMLHttpRequest();
+// URL로 서버 데이타(json) 연결하기
+BankDataRequest.open('GET', bankDataRequestURL);
+BankDataRequest.responseType = 'json';
+// 서버에서 response 요청을 보냄
+BankDataRequest.send();
+
+// onload를 통해 응답이 성공적으로 돌아왔을 때 작동할 코드 작성
+BankDataRequest.onload = function () {
+  const BankList = BankDataRequest.response; // BankDataRequest.response는 서버의 json 파일 객체
+  console.log(BankList);
+};
+
+//데이터 조작
